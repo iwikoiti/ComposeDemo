ab88367c35b2130bfc99fc041cd0070553275241
yui <yuineor@gmail.com>
2025-09-10 18:41:29 +0700

    add composable demotext

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +21,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { }
+                Scaffold(modifier = Modifier.fillMaxSize()) {  }
             }
         }
     }
+}
+
+@Composable
+fun DemoText(message:String, fontSize: Float ) {
+Text(
+    text=message,
+    fontSize=fontSize.sp,
+    fontWeight = FontWeight.Bold
+ )
 }
 
